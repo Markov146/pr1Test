2295de394b7e6c5743a5ab64dbf3ab93bf806144
Markov146 <denismarkov954@gmail.com>
2024-01-27 13:48:00 +0300

    Загружаю проект

---
+++ PasswordValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    public class PasswordValidator
+    {
+        public int VerifyPassword(string password)
+        {
+            int score = 0;
+            if (Regex.IsMatch(password, @"\d"))
+            {
+                score++;
+            }
+            if (Regex.IsMatch(password, @"[a-z]"))
+            {
+                score++;
+            }
+            if (Regex.IsMatch(password, @"[A-Z]"))
+            {
+                score++;
+            }
+            if (Regex.IsMatch(password, @"[!@#$%^&*(),.?\:{ }|<_>]"))
+            {
+                score++;
+            }
+            if (password.Length > 7)
+            {
+                score++;
+            }
+            return score;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp42;
+using System;
+using System.Text.RegularExpressions;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+            PasswordValidator validator = new PasswordValidator();
+            int score = validator.VerifyPassword(password);
+
+            if (score == 5)
+            {
+                Console.WriteLine($"Пароль удовлетворяет всем критериям безопасности. Баллы: {score}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Пароль не удовлетворяет всем критериям безопасности. Баллы: {score}");
+                Console.WriteLine("Попробуйте снова");
+            }
+        }
+    }
+}
